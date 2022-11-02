e9650be51d9ed1d6325b1f4001c22b621cc7a574
Felipe Costa <felipecosta8449@hotmail.com>
2022-11-02 16:58:51 -0300

    plot circle

--- Computational_Statistics/Assignments/T1/Code/circle.py
+++ Computational_Statistics/Assignments/T1/Code/circle.py
@@ -1,42 +1,37 @@
 """Script to make a circle."""
-# https://stackoverflow.com/questions/56719988/altair-plot-circle-ellipse-annotations-in-faceted-chart
 
 import pandas as pd
 import numpy as np
 import altair as alt
 
-# make some data to test
 
-N = 1000
-df = pd.DataFrame(
-    {
-        "x1": np.random.normal(0, 1, N),
-        "x2": np.random.normal(0, 1, N),
-        "facet": np.random.choice(["a", "b", "c"], N),
-    }
-)
+def circle(x, y, radius, points):
+    """Create a circle.
 
-# derived variables
-df["y1"] = np.where(np.sqrt(df["x1"] ** 2 + df["x2"] ** 2) > 2, "f", "p")
-df["y2"] = 0.5 * df["x2"] + 2 + np.random.normal(0.5, N)
-df["color"] = np.where(df["y1"].eq("f"), "red", "green")
-
-# custom color map
-domain = ["f", "p"]
-range_ = ["red", "green"]
-
-# create and render chart
-base_chart = (
-    alt.Chart(df)
-    .mark_circle(opacity=1, size=15)
-    .encode(
-        alt.X("x1", scale=alt.Scale(domain=(-4, 4))),
-        alt.Y("x2", scale=alt.Scale(domain=(-4, 4))),
-        color=alt.Color("y1", scale=alt.Scale(domain=domain, range=range_)),
-        facet=alt.Facet("facet", columns=3),
+    x, y: center of the circle
+    """
+    theta = np.linspace(0, 2 * np.pi, points)
+    df_circle = pd.DataFrame(
+        {
+            "x": x + radius * np.cos(theta),
+            "y": y + radius * np.sin(theta),
+        }
     )
-    .properties(width=200, height=200)
-    .resolve_scale(color="independent")
-)
+    df_circle["index"] = df_circle.index
+    return df_circle
 
-base_chart.save("circle.html")
+
+def plot_circle(x, y, radius, color, points):
+    """Plot a circle."""
+    c = circle(x, y, radius, points)
+    chart_circle = (
+        alt.Chart(c)
+        .mark_line(color=color)
+        .encode(
+            alt.X("x", scale=alt.Scale(zero=False)),
+            alt.Y("y", scale=alt.Scale(zero=False)),
+            order="index",
+        )
+    )
+
+    chart_circle.save("circle.html")
