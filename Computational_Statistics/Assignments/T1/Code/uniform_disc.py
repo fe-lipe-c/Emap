cecfd8ecd9b50823f8efa9022944f33e8057852b
Felipe Costa <felipecosta8449@hotmail.com>
2022-11-02 17:04:23 -0300

    uniform on a disc

---
+++ Computational_Statistics/Assignments/T1/Code/uniform_disc.py
@@ -0,0 +1,9 @@
+"""Script to draw samples of a uniform distribution on a disc."""
+# https://math.stackexchange.com/questions/499041/probability-density-of-continuous-uniform-distribution-over-the-unit-circle
+
+import numpy as np
+import pandas as pd
+
+
+def uniform_disc_dist(size, radius):
+    """Uniform distribution on a disc."""
